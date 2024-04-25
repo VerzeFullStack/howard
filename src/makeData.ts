b0f51f83fcf8c915266c84b84ceb54b461824bb5
Howard-P <152210081+Howard-P@users.noreply.github.com>
2024-04-25 02:00:00 -0400

    Table branch (#8)
    * added subcomponent rowcanexpand
    
    * changed named fetchData -> makeData
    
    * deleted no need style

--- src/makeData.ts
+++ src/makeData.ts
@@ -2,13 +2,23 @@ import { faker } from '@faker-js/faker'
 
 export type Person = {
   firstName: string
-  lastName: string
+  lastName: string | undefined
   age: number
-  visits: number
+  visits: number | undefined
   progress: number
   status: 'relationship' | 'complicated' | 'single'
+  rank: number
+  createdAt: Date
   subRows?: Person[]
 }
+const relationship = { id: 1, name: "On Deck", color: "blue.300" };
+const complicated = {id: 2, name: "In Progress", color: "yellow.400"};
+const single = { id: 3, name: "Testing", color: "pink.300" };
+export const STATUSES = [
+  relationship,
+  complicated,
+  single,
+];
 
 const range = (len: number) => {
   const arr: number[] = []
@@ -21,22 +31,24 @@ const range = (len: number) => {
 const newPerson = (): Person => {
   return {
     firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
+    lastName: Math.random() < 0.1 ? undefined : faker.person.lastName(),
     age: faker.number.int(40),
-    visits: faker.number.int(1000),
+    visits: Math.random() < 0.1 ? undefined : faker.number.int(1000),
     progress: faker.number.int(100),
+    createdAt: faker.date.anytime(),
     status: faker.helpers.shuffle<Person['status']>([
       'relationship',
       'complicated',
       'single',
     ])[0]!,
+    rank: faker.number.int(100),
   }
 }
 
 export function makeData(...lens: number[]) {
   const makeDataLevel = (depth = 0): Person[] => {
     const len = lens[depth]!
-    return range(len).map((_d): Person => {
+    return range(len).map((): Person => {
       return {
         ...newPerson(),
         subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
